30935eff9a834784a2cd7cf3737b0c89b8b4909e
Luis Daniel De la Rosa <daniel@Mac-mini-de-Luis.local>
2025-07-27 17:08:02 -0600

    TrustProxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies('*');
+        $middleware->trustProxies(
+                '*', 
+                Illuminate\Http\Request::HEADER_X_FORWARDED_ALL
+            );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
