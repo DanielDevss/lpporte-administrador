b9c0626d3db842cd59155fbe599d4d8f8e5b798d
Luis Daniel de la Rosa <luisdaniel.dlr.380@gmail.com>
2025-08-09 10:34:57 -0600

    feat(stats): stats dinamics

--- app/Filament/Admin/Widgets/SummaryStats.php
+++ app/Filament/Admin/Widgets/SummaryStats.php
@@ -2,26 +2,47 @@
 
 namespace App\Filament\Admin\Widgets;
 
+use App\Enums\PaymentIntentStatusEnum;
+use App\Models\Customer;
+use App\Models\Order;
+use App\Models\Product;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class SummaryStats extends BaseWidget
 {
+    protected function getHeading(): string|null
+    {
+        return "Estadisiticas de tienda";
+    }
     protected function getStats(): array
     {
         return [
-            Stat::make("Clientes Activos", "45")
+            Stat::make("Clientes Activos", Customer::count())
                 ->icon('heroicon-m-user')
                 ->url(route('filament.admin.resources.categories.index'))
                 ->description('Presiona para ir a clientes'),
-            Stat::make("Clientes Suscritos", "22")
+            Stat::make("Clientes Suscritos", Customer::where('suscription_active', true)->where('suscription_id', '>', '1')->count())
                 ->icon('heroicon-m-user-plus')
-                ->url('filament.admin.resources.categories.index')
+                ->url(route('filament.admin.resources.customers.index'))
                 ->description('Presiona para ir a clientes'),
-            Stat::make("Productos Publicos", value: "32")
+            Stat::make("Productos Publicos", value: Product::publics()->count())
                 ->icon('heroicon-m-shopping-bag')
                 ->url(route('filament.admin.resources.products.index'))
                 ->description('Presiona para ir a productos'),
+            Stat::make(
+                "Ventas pendientes",
+                value: Order::where(
+                    'status',
+                    '!=',
+                    PaymentIntentStatusEnum::Succeeded->value
+                )
+                    ->where('status', '!=', PaymentIntentStatusEnum::Canceled->value)
+                    ->count()
+            )
+                ->icon('heroicon-m-clock')
+                ->url(route('filament.admin.resources.orders.index'))
+                ->description('Presiona para ir a ventas'),
         ];
     }
 }
