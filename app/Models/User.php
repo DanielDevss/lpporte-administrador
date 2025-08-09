bf0b0c07c810f9b3cc01b9acb22dcbb93fa6fcf3
Luis Daniel de la Rosa <luisdaniel.dlr.380@gmail.com>
2025-08-09 09:51:16 -0600

    feat(user): access panel not customer

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasApiTokens;
@@ -39,7 +41,7 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
-    
+
     protected function casts(): array
     {
         return [
@@ -48,7 +50,16 @@ class User extends Authenticatable
         ];
     }
 
-    public function customer () {
+    public function customer()
+    {
         return $this->hasOne(Customer::class, 'user_id');
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        if ($panel->getId() === 'admin') {
+            return $this->customer()->doesntExist();
+        }
+        return true;
     }
 }
